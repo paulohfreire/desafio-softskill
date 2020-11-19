cd9049102c33f2e3853298b4f9dee04cc7e84c4e
Paulo Henrique Freire <paulo.freire53@gmail.com>
2020-11-19 10:23:41 -0300

    Initial commit

---
+++ src/pages/Landing/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Title } from './styles';
+const Landing: React.FC = () => {
+
+    return (
+        <>
+        <Title>Hello World</Title>
+        </>
+    )}
+export default Landing;
---
+++ src/pages/Landing/styles.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const Title = styled.h1`
+    font-size: 48px;
+    color: #3a3a3a;
+    max-width: 450px; // Largura do elemento na exibição
+    line-height: 56px;  // espaçamento entre linhas
+    margin-top: 80px;  // Distancia do elemento para o topo ou para o elemento de cima
+
+`;
